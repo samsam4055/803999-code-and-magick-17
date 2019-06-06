b4f1fe6a2e48ebf2c8e9a28fe0c984eab73bf2e6
samsam4055 <39153320+samsam4055@users.noreply.github.com>
2019-06-06 15:31:45 +0300

    Правки по линтеру

--- js/stat.js
+++ js/stat.js
@@ -11,12 +11,12 @@ var BAR_WIIDTH = 40;
 var GAP_X = 50;
 var MESSAGE_Y = CLOUD_Y + FONT_GAP;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -31,7 +31,7 @@ var getRandomNumber = function (min, max) {
   return Math.random() * (max - min) + min;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
@@ -48,13 +48,13 @@ window.renderStatistics = function(ctx, names, times) {
     ctx.fillStyle = '#000';
     ctx.fillText(names[i], CLOUD_X + GAP_X + (GAP_X + BAR_WIIDTH) * i, CLOUD_Y + MESSAGE_Y + GAP + FONT_GAP);
 
-    if (names[i] == "Вы") {
+    if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomNumber(0.1, 1) + ')';
     }
 
-    ctx.fillRect(CLOUD_X + GAP_X + (GAP_X + BAR_WIIDTH) * i, CLOUD_Y + MESSAGE_Y + FONT_GAP * 2 + GAP, BAR_WIIDTH, FONT_GAP + (BAR_HEIGHT * times[i]) / maxTime );
+    ctx.fillRect(CLOUD_X + GAP_X + (GAP_X + BAR_WIIDTH) * i, CLOUD_Y + MESSAGE_Y + FONT_GAP * 2 + GAP, BAR_WIIDTH, FONT_GAP + (BAR_HEIGHT * times[i]) / maxTime);
     ctx.fillStyle = '#000';
     ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP_X + (GAP_X + BAR_WIIDTH) * i, CLOUD_Y + MESSAGE_Y + GAP + FONT_GAP * 3 + (BAR_HEIGHT * times[i]) / maxTime);
   }
